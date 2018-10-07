e061c80f98a2a6dde632e7276911ab11266394a2
YJ C <yjc@dhcp-rhodes-3094.redrover.cornell.edu>
2018-10-06 20:50:19 -0400

    T001 modified

--- src/leetcode/T001.java
+++ src/leetcode/T001.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class T001 {
 	public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
@@ -14,4 +17,27 @@ public class T001 {
         }
         return res;
     }
+
+
+// time complexity : O(n^2)
+
+//optimized solution:
+
+    public int[] twoSum2(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+            }
+            map.put(nums[i],i);
+        }            
+        return res;
+    }
 }
+
+//TO LEARN:
+// 1. map find the key: map.containsKey(xxx)
+// 2. map get value: map.get(key)
+// 3. map put new element: map.put(key, value)
