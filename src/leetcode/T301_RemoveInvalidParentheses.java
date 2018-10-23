0626b6271394575fe48cd151a18dbf3ea41ef828
YJ C <yjc@YJs-MacBook-Pro.local>
2018-10-22 21:36:59 -0500

    Oct-22-fun

--- src/leetcode/T301_RemoveInvalidParentheses.java
+++ src/leetcode/T301_RemoveInvalidParentheses.java
@@ -66,3 +66,73 @@ public class T301_RemoveInvalidParentheses {
     }
 }
 
+
+////////
+
+class Solution {
+    static int maxLen = 0;
+    public List<String> removeInvalidParentheses(String s) {
+        maxLen = 0;
+        // step 1 : find all the combination
+        List<String> res = new ArrayList<>();
+        
+    
+        // step 3 : print set it out as a list
+        for(String str : comb(s)){
+            res.add(str);
+        }
+        return res;
+    }
+    
+    // find all the combination of the string
+    public Set<String> comb(String s){
+        char[] strArr = s.toCharArray();
+        Set<String> res = new HashSet<>();
+        dfs(strArr, new StringBuffer(), 0, res);
+        return res;
+        
+        
+    }
+    
+    
+    
+     public void dfs(char[] strArr, StringBuffer temp, int ptr, Set<String> res){
+            
+	        if(ptr == strArr.length) {
+                if(!isValid(temp,1)) return;
+                if(temp.length()==maxLen)             
+	        	    res.add(temp.toString());
+                else if(temp.length() > maxLen){
+                        maxLen = temp.length();
+                        res.clear();
+                        res.add(temp.toString());
+                    }
+	        	return;
+	        }	
+         
+            // check whether is valid
+         
+            if(!isValid(temp,0)) return;
+         
+	        temp.append(strArr[ptr]);	        
+	        dfs(strArr, temp, ptr+1,res);
+	        
+	        temp.deleteCharAt(temp.length()-1);
+	        dfs(strArr, temp, ptr+1, res);	        
+	}
+    
+    // check whether it is a valid string
+    public boolean isValid(StringBuffer str, int indic){
+        int count = 0;
+        char[] arr=new String(str).toCharArray();
+        for(char ch: arr){
+            if(ch == '(') count++;
+            if(ch == ')') count--;
+            if(count < 0) return false;
+        }
+        return (indic == 1)? count == 0 : true;
+    }
+}
+
+
+
